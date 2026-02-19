d68c927620f863f7975aa6f6194bdaebbfa280df
noeliacabezas-blip <noelia.cabezas@alumnos.ui1.es>
2026-02-19 19:02:46 +0100

    Add files via upload

---
+++ scripts/scr_comunes.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+	
+	// Código que gestiona los botones del menú de navegación que aumentan o disminuyen el tamaño de la fuente
+	var tamFuente = 1; // Tamaño base de la fuente en unidades em	
+	$("#btn-aumentar").on("click", function(event){		
+		tamFuente += 0.05;
+        $("main").css("font-size", tamFuente + "em");
+	});
+	$("#btn-disminuir").on("click", function(event){
+		if (tamFuente > 0.7){ // Que no disminuya menos de 0.7em la fuente para que no se haga demasiado pequeño el texto
+			tamFuente -= 0.05;
+			$("main").css("font-size", tamFuente + "em");
+		}
+	});	
+	
+	
+	//Código que actualiza la fecha y hora en el footer
+	function actualizaHora(){
+		 var fecha = new Date();
+        
+        // Obtener fecha y hora en formato local 
+        var fechaAhora = fecha.toLocaleString();        
+       
+        // Escribe la fecha en el footer
+        $("#txt-fecha").text(fechaAhora);        
+    }
+
+    // Ejecutar la función al cargar la página
+    actualizaHora();
+	
+    // Configurar para que se repita cada 1000 milisegundos (1 segundo)
+    setInterval(actualizaHora, 1000);	
+});
+
+
+
---
+++ scripts/scr_curso_detalle.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){	
+	$(".btn-primary").on("click", function(){
+		window.print();
+	});
+});
